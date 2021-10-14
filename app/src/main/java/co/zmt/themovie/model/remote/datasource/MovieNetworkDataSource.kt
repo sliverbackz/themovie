fa9a84bcc316a56b756992fa3771924a23b4bb5c
Zin Min Tun <zinmintuncu@gmail.com>
2021-10-15 01:02:56 +0630

    feat:Fix api response with flow.

--- app/src/main/java/co/zmt/themovie/model/remote/datasource/MovieNetworkDataSource.kt
+++ app/src/main/java/co/zmt/themovie/model/remote/datasource/MovieNetworkDataSource.kt
@@ -6,14 +6,18 @@ import co.zmt.themovie.model.remote.response.MovieGenreResponse
 import co.zmt.themovie.model.remote.response.PopularMovieResponse
 import co.zmt.themovie.model.remote.response.UpcomingMovieResponse
 import co.zmt.themovie.model.remote.service.MovieService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class MovieNetworkDataSource @Inject constructor(
     private val movieService: MovieService
 ) : BaseDataSource() {
-    suspend fun getUpcomingMovies(): Resource<UpcomingMovieResponse> {
-        return getResult {
-            movieService.getUpcomingMovies(1)
+
+    suspend fun getUpcomingMoviesFlow(): Flow<Resource<UpcomingMovieResponse>> {
+        return flow {
+            emit(Resource.Loading(isLoading = true))
+            emit(getResult { movieService.getUpcomingMovies(1) })
         }
     }
 
