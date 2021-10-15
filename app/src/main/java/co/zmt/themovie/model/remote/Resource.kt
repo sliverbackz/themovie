635e8626daac4076aff936a02d0e13650d4d60a0
Zin Min Tun <zinmintuncu@gmail.com>
2021-10-16 01:11:13 +0630

    fix:Resource changes

--- app/src/main/java/co/zmt/themovie/model/remote/Resource.kt
+++ app/src/main/java/co/zmt/themovie/model/remote/Resource.kt
@@ -4,8 +4,7 @@ sealed class Resource<out T> {
 
     data class Success<out T>(val data: T) : Resource<T>()
 
-    data class Error<out T>(val message: String, val throwable: Throwable, val data: T?) :
-        Resource<T>()
+    data class Error<out T>(val message: String, val throwable: Throwable) : Resource<T>()
 
     data class Loading<out T>(val isLoading: Boolean = true) : Resource<T>()
 
