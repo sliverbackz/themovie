c2ae71d5792baf692a73cb570c61ab74e2af59f3
Zin Min Tun <zinmintuncu@gmail.com>
2021-10-16 01:10:42 +0630

    feat:Add RepositoryModule.kt

--- app/src/main/java/co/zmt/themovie/di/RepositoryModule.kt
+++ app/src/main/java/co/zmt/themovie/di/RepositoryModule.kt
@@ -1,21 +1,32 @@
 package co.zmt.themovie.di
 
+import co.zmt.themovie.model.local.datasource.MovieLocalDataSource
+import co.zmt.themovie.model.remote.datasource.MovieNetworkDataSource
+import co.zmt.themovie.repository.MovieRepository
+import co.zmt.themovie.repository.entitymapper.MovieEntityMapper
+import co.zmt.themovie.repository.entitymapper.MovieGenreEntityMapper
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
 
-//@Module
-//@InstallIn(ViewModelComponent::class)
-//object RepositoryModule {
-//    @Provides
-//    @ViewModelScoped
-//    fun provideDiscoverRepository(
-//        discoverService: TheDiscoverService,
-//        movieDao: MovieDao,
-//        tvDao: TvDao
-//    ): DiscoverRepository {
-//        return DiscoverRepository(discoverService, movieDao, tvDao)
-//    }
-//}
+@Module
+@InstallIn(ViewModelComponent::class)
+object RepositoryModule {
+    @Provides
+    @ViewModelScoped
+    fun provideMovieRepository(
+        movieNetworkDataSource: MovieNetworkDataSource,
+        movieLocalDataSource: MovieLocalDataSource,
+        movieEntityMapper: MovieEntityMapper,
+        movieGenreEntityMapper: MovieGenreEntityMapper
+    ): MovieRepository {
+        return MovieRepository(
+            movieNetworkDataSource,
+            movieLocalDataSource,
+            movieEntityMapper,
+            movieGenreEntityMapper
+        )
+    }
+}
